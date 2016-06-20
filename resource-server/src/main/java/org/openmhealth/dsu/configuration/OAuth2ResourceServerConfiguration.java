ea9a8925c9bbfefbc6a62aaddffa23d4af7362e1
mdashadmin <mdashadmin@mdash.cs.vassar.edu>
2016-06-20 17:33:12 -0400

    Implementing web-app with Spring Security, added a resource server endpoint for testing

--- resource-server/src/main/java/org/openmhealth/dsu/configuration/OAuth2ResourceServerConfiguration.java
+++ resource-server/src/main/java/org/openmhealth/dsu/configuration/OAuth2ResourceServerConfiguration.java
@@ -28,6 +28,12 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
  *
  * @author Emerson Farrugia
  */
+
+@RequestMapping("/") // Included a resource endpoint for testing purposes
+public String home() {
+    return "Hello, World!";
+}
+
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
